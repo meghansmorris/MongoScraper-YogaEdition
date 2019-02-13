c28eaab7d71ba2745e86e19ec9f11ea102416ec1
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-02-13 16:07:54 -0700

    styling and formatting, articles are properly scraping to the page now

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,32 +1,31 @@
-function displayArticles(data) {
-    $("#articleList").empty();
-    data.forEach(function(scrapedArticles) {
-        var articleContainer = $("#articleList");
-        articleContainer.addClass("card");
 
-        var image = $(`<img src=${scrapedArticles.image} class="card-img-top">`);
-        var cardBody = $(`<div data-id=${scrapedArticles._id} class="card-body">`);
-        var cardTitle = $(`<h5 class="card-title">${scrapedArticles.title}</h5>`);
-        var cardAuthor = $(`<p class="card-text">${scrapedArticles.author}</p>`);
-        var cardLink = $(`<p class="card-text">${scrapedArticles.link}</p>`);
-
-        articleContainer.append(image);
-        articleContainer.append(cardBody);
-        articleContainer.append(cardTitle);
-        articleContainer.append(cardAuthor);
-        articleContainer.append(cardLink);
-
-    })
-};
-
-$.getJSON("/scrape", function(data) {
-    displayArticles(data);
+$.getJSON("/articles", function(data) {
     console.log(data);
+    $("#articleList").empty();
+
+    data.forEach(function(data) {        
+        var articleContainer = $("#articleList");
+        
+        var card = $(`<div class="card"></div>`)
+        var image = $(`<img src=${data.image} class="card-img-top">`);
+        var cardBody = $(`<div data-id=${data._id} class="card-body">`);
+        var cardTitle = $(`<h5 class="card-title">${data.title}</h5>`);
+        var cardAuthor = $(`<p class="card-text">${data.author}</p>`);
+        var cardLink = $(`<p class="card-text">${data.link}</p>`);
+
+        articleContainer.append(card);
+        card.append(image);
+        card.append(cardBody);
+        card.append(cardTitle);
+        card.append(cardAuthor);
+        card.append(cardLink);
+    });
+
 });
 
-$("#scrapebtn").on("click", function() {
-    $.getJSON("/scrape", function(data) {
-        displayArticles(data);
-    })
-});
+// $("#scrapebtn").on("click", function() {
+//     $.getJSON("/scrape", function(data) {
+//         displayArticles(data);
+//     })
+// });
 
